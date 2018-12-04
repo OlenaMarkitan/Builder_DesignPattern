543776d4a2770ad96023ae291d225ed2792b3282
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-04 23:32:18 +0200

    Add files via upload

---
+++ Coke.java
@@ -0,0 +1,16 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Builder.Builder_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+
+public class Coke extends ColdDrink {
+
+    @Override
+    public float price() {
+        return 30.0f;
+    }
+
+    @Override
+    public String name() {
+        return "Coke";
+    }
+}
