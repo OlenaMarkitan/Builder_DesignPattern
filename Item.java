a4c491c58f482415eb63a7b42a4207947ad35465
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-04 23:32:55 +0200

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,9 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Builder.Builder_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+
+public interface Item {
+    public String name();
+    public Packing packing();
+    public float price();
+}
