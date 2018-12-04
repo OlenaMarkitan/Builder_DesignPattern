0316a80f8dd1b5b54dd5b97c8ec854febd36c1f0
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-04 23:34:05 +0200

    Add files via upload

---
+++ Packing.java
@@ -0,0 +1,7 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Builder.Builder_tutorialpoints;
+
+// https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+
+public interface Packing {
+    public String pack();
+}
