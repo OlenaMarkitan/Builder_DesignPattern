0e0bc09567dc5c36921aa6bc203780600c6815e7
OlenaMarkitan <olena.markitan@gmail.com>
2018-12-04 23:31:16 +0200

    Add files via upload

---
+++ Bottle.java
@@ -0,0 +1,12 @@
+package com.marlen.java.lesson_1.Lesson7_1.Home7.e_olymp.DesignPatterns.Creational_Patterns.Builder.Builder_tutorialpoints;
+
+
+// https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+
+public class Bottle implements Packing {
+
+    @Override
+    public String pack() {
+        return "Bottle";
+    }
+}
